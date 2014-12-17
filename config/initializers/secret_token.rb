a9063ebccc99d0807f998ae241f541ab059828b2
vinay-manjunath <vinaym@carmatec.com>
2014-12-18 01:19:14 +0530

    all files commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BankingApp::Application.config.secret_key_base = '4f05125432547804323aa05b3c07517acb13ac1fd6bdca24941cb94846c3cba4ed6626b2f428b5302487fc8fc2e8acd5315f84f31399f088cd64ec448bf5acb8'
